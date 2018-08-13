11a1c8f0fa051ea87e17d5ec0fb2e2f89646019e
Jared Lander <info@jaredlander.com>
2018-08-13 11:54:08 -0400

    Fit a glmnet

--- code/glmnet.r
+++ code/glmnet.r
@@ -17,3 +17,17 @@ valueFormula <- TotalValue ~ FireService +
     LotFront + LotDepth + BldgFront + 
     BldgDepth + LotType + Landmark + BuiltFAR +
     Built + HistoricDistrict - 1
+
+valueFormula
+class(valueFormula)
+
+value1 <- lm(valueFormula, data=land_train)
+coefplot(value1, sort='magnitude')
+
+landX_train <- build.x(valueFormula, data=land_train, contrasts=FALSE, sparse=TRUE)
+landX_train
+landY_train <- build.y(alueFormula, data=land_train)
+head(landY_train, n=20)v
+
+value2 <- glmnet(x=landX_train, y=landY_train, family='gaussian')
+coefpath(value2)
