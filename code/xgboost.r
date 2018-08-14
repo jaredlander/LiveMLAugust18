64389a59446ca3eecbb3642bc7c12e896f513a8e
Jared Lander <info@jaredlander.com>
2018-08-14 12:01:31 -0400

    Showed early stopping

--- code/xgboost.r
+++ code/xgboost.r
@@ -77,3 +77,19 @@ xg5 <- xgb.train(
     watchlist=list(train=xgTrain, validate=xgVal),
     print_every_n=1
 )
+
+xg5$evaluation_log
+dygraph(xg5$evaluation_log)
+
+xg6 <- xgb.train(
+    data=xgTrain,
+    objective="binary:logistic",
+    nrounds=500,
+    eval_metric='logloss',
+    watchlist=list(train=xgTrain, validate=xgVal),
+    print_every_n=1,
+    early_stopping_rounds=70
+)
+dygraph(xg6$evaluation_log)
+xg6$best_iteration
+xg6$best_score
